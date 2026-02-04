44b3d2feda0c30f18a1b5598be9003fa53d1b565
SteveMCWin <stevp003@gmail.com>
2026-02-04 01:20:25 +0100

    I might've had a breakthrough. All it cost me was like 2 hours of sleep

--- settings.go
+++ settings.go
@@ -7,19 +7,24 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"tuner/tuning"
+
+	"github.com/charmbracelet/lipgloss/v2"
 )
 
 type SettingsData struct {
 	Tunings      map[string]tuning.Tuning `json:"tunings"`
 	ColorThemes  map[string]ColorTheme    `json:"color_themes"`
 	BorderStyles map[string]string        `json:"border_styles"`
-	AsciiArt     map[string]string        `json:"ascii_art"`
+	AsciiArt     map[string]string        // NOTE: not loaded from json but by looking at the art dir
 }
 
 type SettingsOptions struct {
-	Description string   `json:"description"`
-	Options     []string `json:"options"`
+	Name        string
+	Description string
+	Options     []string
+	Previews    []string
 }
 
 type AppSettings struct {
@@ -29,6 +34,72 @@ type AppSettings struct {
 	SelectedTheme    string `json:"selected_theme"`
 	// Tunings map[string]tuning.Tuning `json:"tunings"`
 	// ColorThemes map[string]ColorTheme `json:"themes"`
+}
+
+func DefineSettingsOptions(data SettingsData) []SettingsOptions {
+	ascii_art := SettingsOptions{
+		Name:        "Ascii Art",
+		Description: "The character art displayed on the left side of the terminal when tuning is in progress. Purely for aesthetical purposes, but I spent a lot of time drawing it :^)",
+		Options:     make([]string, 0),
+		Previews:    make([]string, 0),
+	}
+
+	for k, v := range data.AsciiArt {
+		ascii_art.Options = append(ascii_art.Options, k)
+		ascii_art.Previews = append(ascii_art.Previews, v)
+	}
+
+	borders := SettingsOptions{
+		Name:        "Border Style",
+		Description: "The appearance of displayed borders. The double borders are my favourite, that's why it's the default one hihi.",
+		Options:     make([]string, 0),
+		Previews:    make([]string, 0),
+	}
+
+	for k, v := range data.BorderStyles {
+		borders.Options = append(borders.Options, k)
+		borders.Previews = append(borders.Previews, v) // TODO: remember to add some border examples for the previews
+	}
+
+	themes := SettingsOptions{
+		Name:        "Color Theme",
+		Description: "Colors used for displaying the user interface. Comprised of 3 colors each. Affects only the foreground elements.",
+		Options:     make([]string, 0),
+		Previews:    make([]string, 0),
+	}
+
+	for k, v := range data.ColorThemes {
+		themes.Options = append(themes.Options, k)
+		blocks := `
+		██████
+		██████
+		`
+		tmp_style := lipgloss.NewStyle()
+		preview := tmp_style.Foreground(lipgloss.Color(v.Primary)).Render(blocks)
+		preview += tmp_style.Foreground(lipgloss.Color(v.Secondary)).Render(blocks)
+		preview += tmp_style.Foreground(lipgloss.Color(v.Tertiary)).Render(blocks)
+		themes.Previews = append(themes.Previews, preview)
+	}
+
+	tunings := SettingsOptions{
+		Name:        "Displayed Tuning",
+		Description: "A tuning that will be displayed along the ascii art. Mostly there for aesthetical reasons but also quite handy if you don't have them memorized exactly.",
+		Options:     make([]string, 0),
+		Previews:    make([]string, 0),
+	}
+
+	for k, v := range data.Tunings {
+		themes.Options = append(themes.Options, k)
+		var builder strings.Builder
+		builder.WriteByte('\n')
+		for _, note := range v.Notes {
+			builder.WriteString(note)
+			builder.WriteByte('\n')
+		}
+		themes.Previews = append(themes.Previews, builder.String())
+	}
+
+	return []SettingsOptions{ascii_art, borders, themes, tunings}
 }
 
 func LoadSettingsData() SettingsData {
